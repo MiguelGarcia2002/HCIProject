25d1323a4e50ba70f41ba2bbeb02bd089b55e23d
Angelica Moreno <angelica.mxrenx@gmail.com>
2024-04-15 00:21:02 -0400

    created branch, coded a portion of the muscle groups task

---
+++ pages/muscleGroups.py
@@ -0,0 +1,12 @@
+muscle_groups = {
+    'arms' : ['bicep curls', 'hammer curls', 'tricep kickbacks', 'tricep dips' ],
+    'back' : ['lateral pulldowns', 'seated rows' 'pull-ups', 'bent over rows'],
+    'chest' : ['bench press', 'chest flye', 'push ups', 'chest press machine'],
+    'shoulders' : ['shoulder press', 'dumbbell overhead press', 'lateral raises', 'front raises'],
+    'glutes' : ['hip thrusts', 'bulgarian split squats', 'step-ups', 'cable kickbacks'],
+    'legs' : ['squats', 'romanian deadlifts', 'leg press', 'leg extentsions', 'hamstring curls'],
+}
+
+# function that generates the workout plan
+
+# fucntion that displays the workout plan
